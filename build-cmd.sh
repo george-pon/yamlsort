ed2a2b5302a6ccaf43a045450df295420037ee9c
Jun Obama <george@yk.rim.or.jp>
2018-12-13 23:14:38 +0900

    golang 1.11 go mod init

--- build-cmd.sh
+++ build-cmd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 basedir=$PWD
 echo basedir is $basedir
 
@@ -7,32 +9,52 @@ export GOPATH=$basedir
 echo GOPATH is $GOPATH
 
 # add $basedir/bin to PATH 
-already=$( echo $PATH | sed -e 's/:/\n/g' | grep "$basedir/bin")
-if [ -z "$already" ]; then
+if ! echo $PATH | sed -e 's/:/\n/g' | grep "$basedir/bin" > /dev/null ; then
+    echo "append  $basedir/bin  to PATH"
     export PATH=$PATH:$basedir/bin
 fi
 
 # add $basedir/src/yamlsort to PATH 
-already=$( echo $PATH | sed -e 's/:/\n/g' | grep "$basedir/src/yamlsort")
-if [ -z "$already" ]; then
+if ! echo $PATH | sed -e 's/:/\n/g' | grep "$basedir/src/yamlsort" > /dev/null ; then
+    echo "append  $basedir/src/yamlsort  to PATH"
     export PATH=$PATH:$basedir/src/yamlsort
 fi
 
 if [ $# -eq 0 ]; then
-    mode=build
+    mode=modbuild
 else
     mode=$1
 fi
 
-# check dep command
-type dep
-RC=$?
-if [ $RC -ne 0 ]; then
-    # install dep command
-    go get -u github.com/golang/dep/cmd/dep
+if [ x"$mode"x = x"clean"x ]; then
+    /bin/rm -rf pkg
+    /bin/rm -rf bin
+    find . | grep -v .git/
+fi
+
+if [ x"$mode"x = x"modbuild"x ]; then
+    # for golang 1.11
+    export GO111MODULE=on
+    pushd $GOPATH/src/yamlsort
+        if [ ! -r $basedir/src/yamlsort/go.mod ]; then
+            go mod init
+        fi
+        go vet
+        go install
+        # GOOS=windows GOARCH=amd64 go install
+        # GOOS=linux GOARCH=amd64 go install
+        # GOOS=linux GOARCH=arm64 go install
+        # GOOS=freebsd GOARCH=amd64 go install
+        # GOOS=freebsd GOARCH=arm64 go install
+    popd
 fi
 
 if [ x"$mode"x = x"depbuild"x ]; then
+    # check dep command
+    if ! type dep 2>/dev/null ; then
+        # install dep command
+        go get -u github.com/golang/dep/cmd/dep
+    fi
     pushd $GOPATH/src/yamlsort
         dep init
         go vet
